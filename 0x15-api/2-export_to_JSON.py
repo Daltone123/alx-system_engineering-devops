48c4776b25f485d8001b98d59e08a8b65d14e41d
daltoneotieno123@gmail.com <daltoneotieno123@gmail.com>
2024-07-30 13:29:26 +0300

    Using what you did in the task #0, extend your Python script to export data in the JSON format.

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-Python script that exports data in the JSON format.
-"""
-
+#Python script that exports data in the JSON format.
 from requests import get
 from sys import argv
 import json
